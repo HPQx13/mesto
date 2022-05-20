ea8470c95a78c969629c7ebd207bad7e5f4c7dc1
Vladislav Kuznetsov <kuznetsovx13@yandex.ru>
2022-05-20 14:56:26 +0300

    правки в скриптах и центрирование попапа на разрешении 320

--- scripts/index.js
+++ scripts/index.js
@@ -2,33 +2,30 @@ const popup = document.querySelector('.popup');
 const openPopup = document.querySelector('.popup-open');
 const closePopup = popup.querySelector('.popup__close');
 let nameOutput = document.querySelector('.profile__name');
-let jobOutput = document.querySelector('.profile__description'); 
+let jobOutput = document.querySelector('.profile__description');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__text_type_name');
+let jobInput = document.querySelector('.popup__text_type_job');
 
-
-function togglePopup() {
-    popup.classList.toggle('popup_opened');
+function startPopup() {
+    popup.classList.add('popup_opened');
     nameInput.value = nameOutput.textContent;
     jobInput.value = jobOutput.textContent;
 }
 
-popup.addEventListener('click', function(event) {
-    if (event.target === event.currentTarget) {
-        togglePopup();
-    }
-});
-
-openPopup.addEventListener('click', togglePopup);
-closePopup.addEventListener('click', togglePopup);
-
-let formElement = document.querySelector('.popup__form')
-let nameInput = document.querySelector('.popup__text_type_name');
-let jobInput = document.querySelector('.popup__text_type_job');
+function endPopup() {
+    popup.classList.remove('popup_opened');
+    nameInput.value = nameOutput.textContent;
+    jobInput.value = jobOutput.textContent;
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     nameOutput.textContent = nameInput.value;
     jobOutput.textContent = jobInput.value;
-    togglePopup()
+    endPopup()
 }
 
+openPopup.addEventListener('click', startPopup);
+closePopup.addEventListener('click', endPopup);
 formElement.addEventListener('submit', formSubmitHandler);
